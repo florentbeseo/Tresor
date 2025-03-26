3b01a11d312026458716be60ca61bcea7b026272
Tanguy SUTEAU <tanguy.suteau@reseau.eseo.fr>
2025-03-26 16:41:51 +0100

    Maj player packages.

--- SUTEAU_BIRMELE/base/Packages/Player.c
+++ SUTEAU_BIRMELE/base/Packages/Player.c
@@ -1,3 +1,20 @@
 //
 // Created by florent on 26/03/25.
 //
+#include "Player.h"
+#include <stdbool.h>
+struct Player_s{
+    int position_x;
+    int position_y;
+};
+static Player player;
+
+extern void Player_Init(void){
+  player.position_x = 0;
+  player.position_y = 0;
+};
+
+extern bool Player_Movement(direction dir){
+
+};
+
--- SUTEAU_BIRMELE/base/Packages/Player.h
+++ SUTEAU_BIRMELE/base/Packages/Player.h
@@ -1,8 +1,15 @@
 //
 // Created by florent on 26/03/25.
 //
-
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <stdbool.h>
+
+
+typedef struct Player_s Player;
+
+extern void Player_Init(void);
+extern bool Player_Movement(direction dir);
+extern coordinate Player_Get_pos(void);
 
 #endif //PLAYER_H
